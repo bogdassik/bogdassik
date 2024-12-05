3cf2aa558bf1b401b919c582ab7b5a62a6f11cb9
bogdassik <kuznetsovebogdan@gmail.com>
2024-12-05 18:35:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+// Подключение файла соединения с БД
+include_once 'db.class.php';
 
+// хост БД
+define('db_host','localhost');
+
+// Имя БД
+define('db_name','Kuznetsov');
+
+// Пользователь БД
+define('db_user','admin');
+
+// Пароль БД
+define('db_pass','admin_password');
+
+// Обявление класса для подключения к бд
+$db = new DB_class(db_host,db_name,db_user,db_pass);
+
+$query = $db->select(true, "*","Kuznetsov", "task_id='1'");
