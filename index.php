6d7586b9b5d6e15e0c75008c26bd34ae165aca63
bogdassik <kuznetsovebogdan@gmail.com>
2024-12-12 16:14:10 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once 'db.class.php';
 define('db_host','localhost:3306');
 
 // Имя БД
-define('db_name','Kuznetsov');
+define('db_name','anton');
 
 // Пользователь БД
 define('db_user','admin');
@@ -17,6 +17,38 @@ define('db_pass','admin_password');
 // Обявление класса для подключения к бд
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 
-$query = $db->select(true, "*","Kuznetsovee", "");
-print_r($query);
+$query = $db->select(true,"*","anton1","");
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Мой задачник</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="tasks">
+	<?php
+	for ($i = 0; $i < count($query);$i++) {
+		?>
+		<div class="task">
+			<div class="task-control">
+				<input type="checkbox" name="">
+			</div>
+			<div class="task-text"><?php echo $query[$i]['task_text'] ?></div>
+			<div class="task-date">
+				<input type="date" name="">
+			</div>
+		</div>
+		<?php
+	}
+	?>
+	
+	
+
+</div>
+</body>
+</html>
